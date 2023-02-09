d20e55bf45a66d05c9da524ee57bd613256e35e8
Flavio Nunes <flavionbueri@gmail.com>
2023-02-08 21:47:09 -0300

    Salvando Seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -124,21 +126,48 @@ public class SellerFormController implements Initializable {
 		Seller obj = new Seller();
 		ValidationException exception = new ValidationException("ValidationError");
 
+		//Id
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
 
 		/*
 		 * Se a caixa de texto for nula ou estiver vazia. .trim() é para eliminar
 		 * qualquer espaço em branco que esteja no início ou no final
 		 */
+		
+		//Nome
 		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
 			exception.addError("name", "Field can't be empty");
 		}
-
+		obj.setName(txtName.getText());
+		
+		//Email
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can't be empty");
+		}
+		obj.setEmail(txtEmail.getText());
+		
+		//Data
+		
+		if(dpBirthDate.getValue() == null) {
+			exception.addError("birthDate", "Field can't be empty");
+		}
+		else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault())); // o StartOfDay converte a data que foi pega no computador do usuário para o Instant.
+			obj.setBirthDate(Date.from(instant));
+		}
+		
+		//Salario
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can't be empty");
+		}
+		obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+		obj.setDepartment(comboBoxDepartment.getValue());
+		
 		// Se no map de erros há pelo menos um erro.
 		if (exception.getErrors().size() > 0) {
 			throw exception;
 		}
-		obj.setName(txtName.getText());
+		
 
 		return obj;
 	}
@@ -202,9 +231,10 @@ public class SellerFormController implements Initializable {
 		Set<String> fields = errors.keySet();
 
 		// Se existir a chave name, quer dizer que há um erro.
-		if (fields.contains("name")) {
-			labelErrorName.setText(errors.get("name"));
-		}
+		labelErrorName.setText((fields.contains("name") ? errors.get("name") : ""));
+		labelErrorEmail.setText((fields.contains("email") ? errors.get("email") : ""));
+		labelErrorBirthDate.setText((fields.contains("birthDate") ? errors.get("birthDate") : ""));
+		labelErrorBaseSalary.setText((fields.contains("baseSalary") ? errors.get("baseSalary") : ""));
 	}
 
 	private void initializeComboBoxDepartment() {
